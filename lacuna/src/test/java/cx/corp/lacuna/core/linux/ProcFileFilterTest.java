e3dcc81e21839de94de169a356df04db1b8257f7
cxcorp <joona.o.heikkila@gmail.com>
2017-01-31 01:18:54 +0200

    Add unit tests for ProcFileFilter
    100% branch coverage of ProcFileFilter is not currently achieved, because
    the accept(File file) method will need to be tested via integration tests.
    Also moved a magic literal to LinuxConstants.LOWEST_LEGAL_PID.

---
+++ lacuna/src/test/java/cx/corp/lacuna/core/linux/ProcFileFilterTest.java
@@ -0,0 +1,127 @@
+package cx.corp.lacuna.core.linux;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@RunWith(Parameterized.class)
+public class ProcFileFilterTest {
+
+    @Parameterized.Parameters(name = "{index}: pid_max = {0}")
+    public static Object[] data() {
+        return new Object[] {
+            10,
+            LinuxConstants.FALLBACK_PID_MAX,
+            1 << 22 // pid_max can be as high as PID_MAX_LIMIT (2^22)
+        };
+    }
+
+    @Parameterized.Parameter
+    public int pidMax;
+
+    private ProcFileFilter procFileFilter;
+
+    @Before
+    public void setUp() {
+        pidMax = LinuxConstants.FALLBACK_PID_MAX;
+        procFileFilter = new ProcFileFilter(pidMax);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ctorThrowsIfPassingPidMaxLowerThanLowestLegalPid() {
+        new ProcFileFilter(LinuxConstants.LOWEST_LEGAL_PID - 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void ctorThrowsIfPassingPidMaxEqualToLowestLegalPid() {
+        new ProcFileFilter(LinuxConstants.LOWEST_LEGAL_PID);
+    }
+
+    @Test
+    public void declinesNullFiles() {
+        boolean result = procFileFilter.accept(null);
+        assertFalse(result);
+    }
+
+    @Test
+    public void declinesNonDirectoryFiles() {
+        String procName = generateRandomValidProcName();
+        boolean result = procFileFilter.accept(false, procName);
+        assertFalse("Expected filter to decline non-directory named " + procName, result);
+    }
+
+    @Test
+    public void declinesDirectoriesWithNonIntegerName() {
+        String[] illegalNames = {
+            "/proc/sys/kernel",
+            "/",
+            "..",
+            "-ölok",
+            "1241abc",
+            "asb5123",
+            "XIV",
+            "24F2A"
+        };
+
+        for (String illegalName : illegalNames) {
+            boolean result = procFileFilter.accept(true, illegalName);
+            assertFalse("Expected filter to decline file \"" + illegalName + "\"", result);
+        }
+    }
+
+    @Test
+    public void declinesDirectoriesWithNamesLowerThanLowestLegalPid() {
+        int[] illegalPids = {
+            Short.MIN_VALUE,
+            -1,
+            0
+        };
+
+        for (Integer illegalPid : illegalPids) {
+            boolean result = procFileFilter.accept(true, illegalPid.toString());
+            assertFalse(result);
+        }
+    }
+
+    @Test
+    public void declinesDirectoriesWithNumbersEqualToPidMax() {
+        boolean result = procFileFilter.accept(true, Integer.toString(pidMax));
+        assertFalse(result);
+    }
+
+    @Test
+    public void declinesDirectoriesWithNumbersGreaterThanPidMax() {
+        String[] illegalNames = {
+            (pidMax + 1) + "",
+            (Integer.MAX_VALUE) + "",
+            "1581293129481285417512948151274129849812"
+        };
+
+        for (String illegalName : illegalNames) {
+            boolean result = procFileFilter.accept(true, illegalName);
+            assertFalse(result);
+        }
+    }
+
+    @Test
+    public void acceptsDirectoryWithLowerBoundIntegerName() {
+        boolean result = procFileFilter.accept(true, Integer.toString(LinuxConstants.LOWEST_LEGAL_PID));
+        assertTrue(result);
+    }
+
+    @Test
+    public void acceptsDirectoriesWithLegalIntegerName() {
+        String procName = generateRandomValidProcName();
+        boolean result = procFileFilter.accept(true, procName);
+        assertTrue("Expected name " + procName + " to be accepted!", result);
+    }
+
+    private String generateRandomValidProcName() {
+        return Integer.toString(ThreadLocalRandom.current().nextInt(1, pidMax));
+    }
+}
