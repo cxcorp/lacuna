d3221ecf1da314e0ea05e8afae1f6406449822ac
cxcorp <joona.o.heikkila@gmail.com>
2017-02-04 01:32:28 +0200

    DI process opening to WindowsMemoryReader
    As part of an effort to abstract away kernel32 from WindowsMemoryReader,
    inject a ProcessOpener as a dependency to it. This also fixes leaking
    handles in the class. Removed ProcessAccessException, as it was all in all
    just a dupe for ProcessOpenException.
    
    Still having to maintain the Kernel32 dependency anyways is suboptimal,
    but perhaps we'll come up with something later.
    
    * Inject ProcessOpener to WindowsMemoryReader
    * Replace ProcessAccessException with ProcessOpenException
    * Make ProcessOpenException a RuntimeException

--- lacuna/src/main/java/cx/corp/lacuna/core/windows/WindowsMemoryReader.java
+++ lacuna/src/main/java/cx/corp/lacuna/core/windows/WindowsMemoryReader.java
@@ -5,7 +5,6 @@ import com.sun.jna.Native;
 import com.sun.jna.ptr.IntByReference;
 import cx.corp.lacuna.core.MemoryReadException;
 import cx.corp.lacuna.core.MemoryReader;
-import cx.corp.lacuna.core.ProcessAccessException;
 import cx.corp.lacuna.core.domain.NativeProcess;
 import cx.corp.lacuna.core.windows.winapi.Kernel32;
 import cx.corp.lacuna.core.windows.winapi.ProcessAccessFlags;
@@ -22,6 +21,7 @@ public class WindowsMemoryReader implements MemoryReader {
         ProcessAccessFlags.QUERY_INFORMATION | ProcessAccessFlags.VIRTUAL_MEMORY_READ;
 
     private final Kernel32 kernel;
+    private final ProcessOpener processOpener;
 
     /**
      * Instantiates a new {@link WindowsMemoryReader} instance using the specified
@@ -29,24 +29,21 @@ public class WindowsMemoryReader implements MemoryReader {
      *
      * @param kernel The Kernel32 WindowsAPI proxy used for process memory reading.
      */
-    public WindowsMemoryReader(Kernel32 kernel) {
-        if (kernel == null) {
-            throw new IllegalArgumentException("kernel cannot be null!");
+    public WindowsMemoryReader(ProcessOpener processOpener, Kernel32 kernel) {
+        if (kernel == null || processOpener == null) {
+            throw new IllegalArgumentException("Parameters cannot be null!");
         }
+        this.processOpener = processOpener;
         this.kernel = kernel;
     }
 
     /**
      * {@inheritDoc}
-     * <p>
-     * <p>This method implementation first attempts to open a handle for the specified {@code process}
-     * using the Windows API {@link Kernel32#openProcess(int, boolean, int)} method,
+     * <p>This method implementation first attempts to open a handle for the specified {@code process},
      * then reads the specified memory segment using the
      * {@link Kernel32#readProcessMemory(int, int, Memory, int, IntByReference)} method.
-     * <p>
      * <p>In order to retrieve a {@link NativeProcess} instance, the
      * {@link cx.corp.lacuna.core.NativeProcessEnumerator} classes can be used.
-     * <p>
      * <blockquote><pre>{@code
      * NativeProcessEnumerator enumerator = new WindowsNativeProcessEnumerator(...);
      * List<NativeProcess> processes = enumerator.getProcesses();
@@ -57,7 +54,7 @@ public class WindowsMemoryReader implements MemoryReader {
      *     for (byte b : bytes) {
      *         System.out.println("%X ", b);
      *     }
-     * } catch (ProcessAccessException | MemoryReadException ex) {
+     * } catch (ProcessOpenException | MemoryReadException ex) {
      *     System.err.println(ex.toString());
      *     if (ex.getCause() != null) {
      *         System.err.println("Cause: " + ex.getCause());
@@ -65,49 +62,44 @@ public class WindowsMemoryReader implements MemoryReader {
      * }
      * }</pre></blockquote>
      *
-     * @throws ProcessAccessException if the specified process could not be opened for reading.
+     * @throws ProcessOpenException   if the specified process could not be opened for reading.
      * @throws MemoryReadException    if reading the process memory fails.
      */
     @Override
     public byte[] read(NativeProcess process, int offset, int bytesToRead)
-        throws ProcessAccessException, MemoryReadException {
+        throws ProcessOpenException, MemoryReadException {
 
-        int handle = kernel.openProcess(FLAGS_READMEMORY, false, process.getPid());
-        if (handle == WinApiConstants.NULLPTR) {
-            throw new ProcessAccessException(
-                "Process " + process + " could not be opened for reading! System error: "
-                    + Native.getLastError());
+        try (ProcessHandle handle = processOpener.open(process.getPid(), FLAGS_READMEMORY)) {
+            Memory buffer = new Memory(bytesToRead);
+            IntByReference bytesRead = new IntByReference(0);
+            boolean success = kernel.readProcessMemory(
+                handle.getNativeHandle(),
+                offset,
+                buffer,
+                bytesToRead,
+                bytesRead);
+
+            if (!success) {
+                int errorCode = Native.getLastError();
+                throw createReadExceptionFromErrorCode(errorCode);
+            }
+
+            byte[] ret;
+            try {
+                ret = buffer.getByteArray(0, bytesToRead);
+            } catch (Error err) {
+                // e.g. "Invalid memory access" if reading outside memory bounds.
+                // the Memory class does bounds checking but you can never be sure since
+                // it ultimately calls the natively implemented Native.read method.
+                // This is a bit of a doomsday defensive programming scenario so not sure
+                // how to unit test this one.
+                throw new MemoryReadException(
+                    "An error occurred while reading memory. Use getCause() to get the cause.",
+                    err);
+            }
+
+            return ret;
         }
-
-        Memory buffer = new Memory(bytesToRead);
-        IntByReference bytesRead = new IntByReference(0);
-        boolean success = kernel.readProcessMemory(
-            handle,
-            offset,
-            buffer,
-            bytesToRead,
-            bytesRead);
-
-        if (!success) {
-            int errorCode = Native.getLastError();
-            throw createReadExceptionFromErrorCode(errorCode);
-        }
-
-        byte[] ret;
-        try {
-            ret = buffer.getByteArray(0, bytesToRead);
-        } catch (Error err) {
-            // e.g. "Invalid memory access" if reading outside memory bounds.
-            // the Memory class does bounds checking but you can never be sure since
-            // it ultimately calls the natively implemented Native.read method.
-            // This is a bit of a doomsday defensive programming scenario so not sure
-            // how to unit test this one.
-            throw new MemoryReadException(
-                "An error occurred while reading memory. Use getCause() to get the cause.",
-                err);
-        }
-
-        return ret;
     }
 
     private MemoryReadException createReadExceptionFromErrorCode(int nativeError) {
