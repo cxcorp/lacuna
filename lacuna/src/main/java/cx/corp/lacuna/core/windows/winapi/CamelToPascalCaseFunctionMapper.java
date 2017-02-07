90de4403fd4d6ba4a298564645e5fa9662af0f92
cxcorp <joona.o.heikkila@gmail.com>
2017-02-07 05:47:55 +0200

    Remove dead code from function mapper

--- lacuna/src/main/java/cx/corp/lacuna/core/windows/winapi/CamelToPascalCaseFunctionMapper.java
+++ lacuna/src/main/java/cx/corp/lacuna/core/windows/winapi/CamelToPascalCaseFunctionMapper.java
@@ -9,13 +9,12 @@ import java.lang.reflect.Method;
  * Provides mapping from camelCase Java method names to PascalCase WinAPI names.
  */
 public class CamelToPascalCaseFunctionMapper implements FunctionMapper {
-    private static String capitalizeFirstLetter(String string) {
-        return string.substring(0, 1).toUpperCase() + string.substring(1);
-    }
-
     @Override
     public String getFunctionName(NativeLibrary nativeLibrary, Method method) {
-        String name = method.getName();
-        return name == null || name.length() < 1 ? name : capitalizeFirstLetter(name);
+        return capitalizeFirstLetter(method.getName());
+    }
+
+    private static String capitalizeFirstLetter(String string) {
+        return string.substring(0, 1).toUpperCase() + string.substring(1);
     }
 }
