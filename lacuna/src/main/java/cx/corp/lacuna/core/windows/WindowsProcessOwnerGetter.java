d394ccaad039bddd1eaaecbdbc492f17d3190191
cxcorp <joona.o.heikkila@gmail.com>
2017-02-14 05:52:59 +0200

    Move token owner name finding to own class
    WinApi is so messy to interact with from Java...

--- lacuna/src/main/java/cx/corp/lacuna/core/windows/WindowsProcessOwnerGetter.java
+++ lacuna/src/main/java/cx/corp/lacuna/core/windows/WindowsProcessOwnerGetter.java
@@ -1,11 +1,7 @@
 package cx.corp.lacuna.core.windows;
 
-import com.sun.jna.Native;
-import com.sun.jna.ptr.IntByReference;
 import cx.corp.lacuna.core.windows.winapi.Advapi32;
 import cx.corp.lacuna.core.windows.winapi.CloseHandle;
-import cx.corp.lacuna.core.windows.winapi.SystemErrorCode;
-import cx.corp.lacuna.core.windows.winapi.WinApiConstants;
 
 import java.util.Optional;
 
@@ -13,17 +9,15 @@ public class WindowsProcessOwnerGetter implements ProcessOwnerGetter {
 
     private final ProcessTokenOpener tokenOpener;
     private final TokenUserFinder tokenUserFinder;
-    private final Advapi32 advapi;
-    private final CloseHandle handleCloser;
+    private final TokenOwnerNameFinder tokenOwnerFinder;
 
     public WindowsProcessOwnerGetter(Advapi32 advapi, CloseHandle handleCloser) {
         if (advapi == null || handleCloser == null) {
             throw new IllegalArgumentException("Arguments cannot be null!");
         }
-        this.advapi = advapi;
-        this.handleCloser = handleCloser;
         tokenOpener = new ProcessTokenOpener(advapi, handleCloser);
         tokenUserFinder = new TokenUserFinder(advapi);
+        tokenOwnerFinder = new TokenOwnerNameFinder(advapi);
     }
 
     @Override
@@ -33,63 +27,10 @@ public class WindowsProcessOwnerGetter implements ProcessOwnerGetter {
         }
 
         try (ProcessToken token = tokenOpener.openToken(processHandle)) {
-            return tokenUserFinder.findTokenUser(token).flatMap(this::getUserName);
+            return tokenUserFinder.findTokenUser(token).flatMap(tokenOwnerFinder::getUserName);
         } catch (TokenOpenException ex) {
             // loggerino
             return Optional.empty();
         }
-    }
-
-    private Optional<String> getUserName(Advapi32.TokenUser user) {
-        // includes null terminator!
-        return getUsernameBufferLength(user)
-            .flatMap(bufLen -> lookupTokenUserName(user, bufLen));
-    }
-
-    private Optional<Integer> getUsernameBufferLength(Advapi32.TokenUser user) {
-        IntByReference nameLength = new IntByReference(0);
-        IntByReference domainLength = new IntByReference(0);
-        IntByReference ignored = new IntByReference(0);
-
-        boolean success =
-            advapi.lookupAccountSidW(
-                WinApiConstants.NULL,
-                user.user,
-                null,
-                nameLength,
-                null,
-                domainLength,
-                ignored);
-
-        return !success && callFailedBecauseBufferWasTooSmall()
-            ? Optional.of(nameLength.getValue())
-            : Optional.empty();
-    }
-
-    private boolean callFailedBecauseBufferWasTooSmall() {
-        return Native.getLastError() == SystemErrorCode.INSUFFICIENT_BUFFER.getSystemErrorId();
-    }
-
-    private Optional<String> lookupTokenUserName(Advapi32.TokenUser user, int nameBufferLenWithNullTerminator) {
-        char[] nameBuffer = new char[nameBufferLenWithNullTerminator];
-        IntByReference nameBufNeededLen = new IntByReference(nameBuffer.length);
-        char[] domainBuffer = new char[WinApiConstants.MAX_DOMAIN_NAME_LENGTH];
-        IntByReference domainBufNeededLen = new IntByReference(domainBuffer.length);
-        IntByReference ignored = new IntByReference(0);
-
-        boolean success =
-            advapi.lookupAccountSidW(
-                WinApiConstants.NULL,
-                user.user,
-                nameBuffer,
-                nameBufNeededLen,
-                domainBuffer,
-                domainBufNeededLen,
-                ignored);
-
-        int lengthWithoutNullTerminator = nameBufferLenWithNullTerminator - 1;
-        return success
-            ? Optional.of(new String(nameBuffer, 0, lengthWithoutNullTerminator))
-            : Optional.empty();
     }
 }
