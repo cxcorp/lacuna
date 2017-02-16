f0cabc27bb75831ca89fb3ba7218aa890caac67c
Joona Heikkilä <joona.o.heikkila@gmail.com>
2017-02-17 01:13:25 +0200

    Separate secton links from normal links
    Seeing the page hierarchy helps users when trying to get back to where
    they came from.

--- lacuna/lacuna-ui/src/components/Header.js
+++ lacuna/lacuna-ui/src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { IndexLink } from 'react-router';
 import './Header.css';
 
 const Header = () => (
     <header className='navigation'>
         <div className='navigation__content'>
-            <p className='navigation__title'>lacuna</p>
+            <p className='navigation__title'>
+                <IndexLink to='/' title='Dashboard' className='navigation__title__link'>lacuna</IndexLink>
+            </p>
         </div>
     </header>
 );
