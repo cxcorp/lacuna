37d790272e38f7efcf028e3c92de73e635b1c790
Joona Heikkilä <joona.o.heikkila@gmail.com>
2017-02-24 18:31:19 +0200

    Fix javadoc issue in ui

--- lacuna/lacuna-ui/src/main/java/cx/corp/lacuna/ui/view/View.java
+++ lacuna/lacuna-ui/src/main/java/cx/corp/lacuna/ui/view/View.java
@@ -1,5 +1,13 @@
 package cx.corp.lacuna.ui.view;
 
+/**
+ * Represents a view in the MVP pattern.
+ * @param <TCallbacks> the type of the callbacks used by the view.
+ */
 public interface View<TCallbacks> {
+    /**
+     * Attaches the provided callbacks to this view.
+     * @param callbacks the callbacks.
+     */
     void attach(TCallbacks callbacks);
 }
