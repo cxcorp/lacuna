8a340f9ae42e378f29b2bef5f462dea808604556
Joona Heikkilä <joona.o.heikkila@gmail.com>
2017-03-05 14:33:55 +0200

    Move certain responsibilities to the table model

--- lacuna/lacuna-ui/src/main/java/cx/corp/lacuna/ui/view/NativeProcessNonEditTableModel.java
+++ lacuna/lacuna-ui/src/main/java/cx/corp/lacuna/ui/view/NativeProcessNonEditTableModel.java
@@ -3,13 +3,25 @@ package cx.corp.lacuna.ui.view;
 import cx.corp.lacuna.core.domain.NativeProcess;
 
 import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NativeProcessNonEditTableModel extends DefaultTableModel {
 
     private static final Class<?>[] NATIVEPROCESS_FIELD_TYPES = {Integer.class, String.class, String.class};
+    private final List<NativeProcess> processes = new ArrayList<>();
 
     public void addRow(NativeProcess process) {
+        processes.add(process);
         super.addRow(processToCells(process));
+    }
+
+    public void clearRows() {
+        super.setRowCount(0);
+    }
+
+    public NativeProcess getProcess(int modelIndex) {
+        return processes.get(modelIndex);
     }
 
     private Object[] processToCells(NativeProcess process) {
