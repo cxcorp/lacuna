adfd8c5c53741fedf6bc73f1963146711f35f91d
Joona Heikkilä <joona.o.heikkila@gmail.com>
2017-03-05 14:33:17 +0200

    Further develop bookmark table

--- lacuna/lacuna-ui/src/main/java/cx/corp/lacuna/ui/view/BookmarkComponent.java
+++ lacuna/lacuna-ui/src/main/java/cx/corp/lacuna/ui/view/BookmarkComponent.java
@@ -2,10 +2,13 @@ package cx.corp.lacuna.ui.view;
 
 import cx.corp.lacuna.core.MemoryReader;
 import cx.corp.lacuna.core.MemoryWriter;
+import cx.corp.lacuna.ui.model.Bookmark;
 import cx.corp.lacuna.ui.model.BookmarkModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.util.Objects;
 
 public class BookmarkComponent {
@@ -15,6 +18,7 @@ public class BookmarkComponent {
 
     private JPanel panel;
     private JTable table;
+    private BookmarkTableModel tableModel;
 
     public BookmarkComponent(BookmarkModel model, MemoryReader reader, MemoryWriter writer) {
         Objects.requireNonNull(model, "model cannot be null!");
@@ -30,21 +34,74 @@ public class BookmarkComponent {
         return panel;
     }
 
+    //<editor-fold desc="components">
     private void createPanel() {
-        panel = new JPanel(new BorderLayout());
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         createTable();
+        createButtons();
     }
 
     private void createTable() {
         table = new JTable();
-        createAndSetTableModel();
+        table.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(
+            KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0),
+            "delete"
+        );
+        table.getActionMap().put(
+            "delete",
+            new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    removeSelectedTableRow();
+                }
+            }
+        );
         table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+
+        createAndSetTableModel();
+        createTablePopupMenu();
         JScrollPane scrollPane = new JScrollPane(table);
-        panel.add(scrollPane, BorderLayout.CENTER);
+        panel.add(scrollPane);
+    }
+
+    private void createTablePopupMenu() {
+        JPopupMenu menu = new JPopupMenu();
+        JMenuItem deleteItem = new JMenuItem("Delete");
+        deleteItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
+        deleteItem.addActionListener(l -> removeSelectedTableRow());
+        menu.add(deleteItem);
+        table.setComponentPopupMenu(menu);
+
     }
 
     private void createAndSetTableModel() {
-        BookmarkTableModel model = new BookmarkTableModel();
-        table.setModel(model);
+        tableModel = new BookmarkTableModel();
+        table.setModel(tableModel);
+    }
+
+    private void createButtons() {
+        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+        JButton addRowButton = new JButton("New Bookmark");
+        addRowButton.addActionListener(e -> tableModel.addRow(Bookmark.empty()));
+        buttonsPanel.add(addRowButton);
+        buttonsPanel.setMinimumSize(new Dimension(0, 40)); // RIP different DPIs
+        panel.add(buttonsPanel);
+    }
+    //</editor-fold>
+
+    private void removeSelectedTableRow() {
+        int row = table.getSelectedRow();
+        if (row == -1) {
+            return;
+        }
+        // view index is not necessarily the same as the model's index,
+        // for example if the table is sorted or filtered, the index will differ
+        int modelIndex = table.convertRowIndexToModel(row);
+        tableModel.removeRow(modelIndex);
+    }
+
+    private void refreshValues() {
+
     }
 }
